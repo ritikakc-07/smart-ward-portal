8649351932a7f59cdf23742c229f35dd5ff661c7
Ritika <ritikakc223@gimal.com>
2026-01-03 12:04:46 +0545

    modified announcement

--- backend/routes/announcements.js
+++ backend/routes/announcements.js
@@ -3,20 +3,29 @@ const router = express.Router();
 const fs = require('fs');
 const path = require('path');
 
-const filePath = path.join(__dirname, '../announcements.json');
+// Safe path to the JSON file
+const filePath = path.join(__dirname, '../data/announcements.json');
 
+// GET all announcements
 router.get('/', (req, res) => {
     const data = fs.readFileSync(filePath, 'utf8');
     res.json(JSON.parse(data));
 });
 
+// POST new announcement (admin demo)
 router.post('/', (req, res) => {
-    const newAnnouncement = req.body;
     const data = fs.readFileSync(filePath, 'utf8');
     const announcements = JSON.parse(data);
+
+    const newAnnouncement = {
+        id: announcements.length + 1,   // automatically assign ID
+        ...req.body
+    };
+
     announcements.push(newAnnouncement);
     fs.writeFileSync(filePath, JSON.stringify(announcements, null, 2));
-    res.json({ message: 'Announcement added' });
+
+    res.json({ message: 'Announcement added', newAnnouncement });
 });
 
 module.exports = router;
